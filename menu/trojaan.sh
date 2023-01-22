8ce83260537063d836e52174c4d9f56dc8462ad3
adamcrew <90532227+scriptadamcrew@users.noreply.github.com>
2023-01-22 19:47:26 +0800

    Update trojaan.sh

--- menu/trojaan.sh
+++ menu/trojaan.sh
@@ -438,8 +438,8 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
 			exit 1
 		fi
 	done
-export patchtls=/hacker-trojanwstls
-export patchnone=/hacker-trojanwsntls
+export patchtls=/trickers-trojanwstls
+export patchnone=/trickers-trojanwsntls
 read -p "   Bug Address (Example: www.google.com) : " address
 read -p "   Bug SNI (Example : m.facebook.com) : " sni
 read -p "   Expired (days) : " masaaktif
@@ -525,8 +525,8 @@ export masaaktif="1"
 export exp=$(date -d "$masaaktif days" +"%Y-%m-%d")
 export user=Trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
 
-export patchtls=/hacker-trojanwstls
-export patchnone=/hacker-trojanwsntls
+export patchtls=/trickers-trojanwstls
+export patchnone=/trickers-trojanwsntls
 
 read -p "   Bug Address (Example: www.google.com) : " address
 read -p "   Bug SNI (Example : m.facebook.com) : " sni
@@ -723,8 +723,8 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#trws " "/usr/local/etc/xray/trojan.json")
 			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
-export patchtls=/hacker-trojanwstls
-export patchnone=/hacker-trojanwsntls
+export patchtls=/trickers-trojanwstls
+export patchnone=/trickers-trojanwsntls
 export user=$(grep -E "^#trws " "/usr/local/etc/xray/trojan.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 export harini=$(grep -E "^#trws " "/usr/local/etc/xray/trojan.json" | cut -d ' ' -f 4 | sed -n "${CLIENT_NUMBER}"p)
 export exp=$(grep -E "^#trws " "/usr/local/etc/xray/trojan.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
